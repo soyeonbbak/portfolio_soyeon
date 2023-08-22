20225b248f5d2f31968d687cef4dd01c433b1623
soyeonbbak <137027250+soyeonbbak@users.noreply.github.com>
2023-08-22 18:11:23 +0900

    Add files via upload

---
+++ project_nike/script/header.js
@@ -0,0 +1,4 @@
+const mainMenu = document.querySelectorAll('.main_menu')
+const subMenu = document.querySelectorAll('.sub_menu')
+const subContents = document.querySelectorAll('.sub_contents')
+console.log(mainMenu, subMenu, subContents)
